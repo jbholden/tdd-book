680c814d7ba52edc9456ef07188340aa83e8055a
Brent Holden <brent.l.holden@gmail.com>
2015-01-20 17:24:51 -0500

    functional test checks for input to-do

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,19 +13,36 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith checks out the home page
-        browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
         # notices page title and header
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
 
         # invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                'Enter a to-do item')
+
         # types "Buy peacok feathers" into text box
+        inputbox.send_keys('Buy peakcock feathers')
+
         # hits "enter", page updates, page shows todo item
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows))
+
         # types "Use peacock features to make a fly" into text input
+        self.fail('Finish the test!') 
+
         # page updates, shows both to-do items
         # Edith finds a unique URL for her
         # Edith visits URL
 
 if __name__ == "__main__":
-    unittest.main(warnings='ignore')
+    unittest.main()
+    #unittest.main(warnings='ignore')

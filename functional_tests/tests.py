319f560a4a974fe799e50dbd639cdde7ebd45150
Brent Holden <brent.l.holden@gmail.com>
2015-05-07 14:57:24 -0400

    make funtional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith checks out the home page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # notices page title and header
         self.assertIn('To-Do',self.browser.title)
@@ -32,8 +32,10 @@ class NewVisitorTest(unittest.TestCase):
 
         # types "Buy peacock feathers" into text box
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
-        # hits "enter", page updates, page shows todo item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -45,7 +47,3 @@ class NewVisitorTest(unittest.TestCase):
         # page updates, shows both to-do items
         # Edith finds a unique URL for her
         # Edith visits URL
-
-if __name__ == "__main__":
-    unittest.main()
-    #unittest.main(warnings='ignore')

586201267841bd7a06175f21a0f03c964262e301
Brent Holden <brent.l.holden@gmail.com>
2015-05-09 15:57:28 -0400

    refactored duplicate urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^lists/', include(list_urls)),
     # url(r'^blog/', include('blog.urls')),
-
     #url(r'^admin/', include(admin.site.urls)),
 )

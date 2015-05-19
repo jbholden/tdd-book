4088ad71cc2ed69bc090640b34655c54c43f5c0b
Brent Holden <brent.l.holden@gmail.com>
2015-05-19 15:46:20 -0400

    moved fts to individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,28 @@
+import sys
+from .base import FunctionalTest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+from unittest import skip
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024,768)
+
+        # input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5)
+
+        # start new list, input is nicely centered
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5)

785c751c8aef66ec2effceb8b0e67d2fcd2a9f90
Brent Holden <brent.l.holden@gmail.com>
2015-05-27 15:23:32 -0400

    moved static folder

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -8,7 +8,7 @@ class LoginTest(FunctionalTest):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A Persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -16,15 +16,15 @@ class LoginTest(FunctionalTest):
         # Edith logs in with her email address
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
-            'authentication_email'  #2
-        ).send_keys('edith@mockmyid.com') #3
+            'authentication_email'
+        ).send_keys('edith@mockmyid.com')
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')  #4
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
